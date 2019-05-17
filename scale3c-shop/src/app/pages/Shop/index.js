2c5ca3a9346317861a644b6b88e565d59bdbd5eb
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-05-17 22:42:47 +0300

    works products rendering always + products counting

--- scale3c-shop/src/app/pages/Shop/index.js
+++ scale3c-shop/src/app/pages/Shop/index.js
@@ -13,23 +13,6 @@ class Shop extends React.Component {
     }
   }
 
-  componentDidMount() {
-    const { getProducts, getProductsSuccess, getProductsFailure } = this.props;
-
-    getProducts();
-    fetch("https://boiling-reaches-93648.herokuapp.com/food-shop/products")
-      .then(response => response.json())
-      .then(json => {
-        const products = json.map(product => ({
-          cartCount: 0,
-        }));
-
-        getProductsSuccess(products);
-      })
-      .catch(() => getProductsFailure("Something went wrong"));
-  }
-
-
   render() {
     const { products, updateCartCount } = this.props;
     return (
@@ -60,11 +43,6 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    getProducts: () => dispatch({ type: "FETCH_PRODUCTS" }),
-    getProductsSuccess: payload =>
-      dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload }),
-    getProductsFailure: payload =>
-      dispatch({ type: "FETCH_PRODUCTS_FAILURE", payload }),
     updateCartCount: (id, count) =>
       dispatch({
         type: "UPDATE_PRODUCT_CART_COUNT",
