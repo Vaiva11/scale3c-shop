bbfa0578a4e7880c253b76ad86c0eb129a760463
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-05-18 02:07:34 +0300

    added remove and checkout button in cart page

--- scale3c-shop/src/app/pages/Cart/index.js
+++ scale3c-shop/src/app/pages/Cart/index.js
@@ -3,44 +3,62 @@ import './index.scss';
 import { ProductCartCard } from "../../components"
 import { connect } from "react-redux";
 
-function Cart({ products, updateCartCount }) {
-  const total = products.reduce((result, product) => {
-    return result + product.price * product.cartCount;
-  }, 0);
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      error: null
+    }
+  }
 
-  return (
-    <ProductCartCard className="Line">
-      <div className="Line--name">
-        <p>Photo</p>
-        <p>Name</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total product price</p>
-        <p>Remove</p>
-      </div>
-      {products.map(({ id, name, price, cartCount, image, updateCartCount }) => {
-        return (
-          <div key={id} className="Line--item">
-            <img src={image} alt={name} />
-            <h3>{name}</h3>
-            <p>{price}</p>
-            <p>{cartCount}</p>
-            <p>{cartCount * price}</p>
-            <div className='Remove-product'>
-              <button
-                type="button"
-                className='Remove-product--button'
-              //onClick={e => updateCartCount(id, e.target.value--)}
-              >
-                ❌
+  render() {
+    const { products, updateCartCount } = this.props;
+
+    const total = products.reduce((result, product) => {
+      return result + product.price * product.cartCount;
+    }, 0);
+
+    const checkOut = total ?
+      (<button type="button" className="Check-out"><h2>Check out</h2></button>) :
+      (<div></div>)
+
+
+    return (
+      <ProductCartCard className="Line">
+        <div className="Line--name">
+          <p>Photo</p>
+          <p>Name</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total product price</p>
+          <p>Remove</p>
+        </div>
+        {products.map(({ id, name, price, cartCount, image }) => {
+          return (
+            <div key={id} className="Line--item">
+              <img src={image} alt={name} />
+              <h3>{name}</h3>
+              <p>{price}</p>
+              <p>{cartCount}</p>
+              <p>{cartCount * price}</p>
+              <div className='Remove-product'>
+                <button
+                  type="button"
+                  className='Remove-product--button'
+                  onClick={e => this.props.updateCartCount(id, e.target.value--)}
+                >
+                  ❌
               </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-      <div className="Line-total">{`Total: ${total}`}</div>
-    </ProductCartCard>
-  )
+          );
+        })}
+        <h2 className="Line-total">{`Total: ${total}`}</h2>
+        <p>{checkOut}</p>
+      </ProductCartCard>
+    )
+  }
 }
 
 function mapDispatchToProps(dispatch) {
